89a1670a6d4419f18d985b1ad6b683016901671c
vaishee90 <aardhana@asu.edu>
2017-11-16 19:59:50 -0800

    Updated receive function

--- msgs.h
+++ msgs.h
@@ -20,7 +20,6 @@ port port_array[MAX_PORTS];
 void init_port(int port_number){
 	
 	port_array[port_number].msg = (message*)malloc(MAX_MSG * sizeof(message));	//Each port can have 10 messages, each of size (int 10)
-	//port_array[port_number].msg.sender_port_number = 999;						//default value 
 	port_array[port_number].mutex = CreateSem(1);
 	port_array[port_number].empty = CreateSem(MAX_MSG);
 	port_array[port_number].full = CreateSem(0);
@@ -43,19 +42,16 @@ void send(int port_number, message *port_msg){
 
 }
 
-message* receive(int port_number){
-
-	message *port_msg;
+//Receive routine for port
+void receive(int port_number, message **port_msg){
 
 	P(port_array[port_number].full);
 	P(port_array[port_number].mutex);
 	//
-	port_msg = &(port_array[port_number].msg[port_array[port_number].front]);
+	*port_msg = &(port_array[port_number].msg[port_array[port_number].front]);
 
 	port_array[port_number].front = (port_array[port_number].front + 1) % MAX_MSG; 
 
 	V(port_array[port_number].mutex);
 	V(port_array[port_number].empty);
-
-	return port_msg;
 }
